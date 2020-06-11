653628be02f58d839d42aece2ac05d725b2bec71
justahuman1 <saivalla123@gmail.com>
2020-06-10 19:35:05 -0700

    Initialized Spring Backend
    Added JPA, Hibernate, Spring-web dependencies

---
+++ src/main/java/com/saivalla/mindspace/Answer.java
@@ -0,0 +1,13 @@
+package com.saivalla.mindspace;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Answer {
+    @RequestMapping(method = RequestMethod.GET, path="/answers")
+    public String initialize(){
+        return "Answer Initializer";
+    }
+}
---
+++ src/main/java/com/saivalla/mindspace/Question.java
@@ -0,0 +1,13 @@
+package com.saivalla.mindspace;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Question {
+    @RequestMapping(method = RequestMethod.GET, path="/questions")
+    public String initialize(){
+        return "Question Initializer";
+    }
+}
